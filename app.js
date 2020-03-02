45e40d05335b0adf6cff8b5912d4f063f1fad75c
Oktay Z <email>
2020-03-02 15:37:35 +0300

    implement users route

--- app.js
+++ app.js
@@ -87,11 +87,40 @@ const deleteTour = (req, res) => {
   });
 };
 
-// // app.get('/api/v1/tours/', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// // app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const getAllUsers = (req, res) => {
+  res.status(501).json({
+    status: 'error',
+    message: 'This route not yet defined'
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(501).json({
+    status: 'error',
+    message: 'This route not yet defined'
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(501).json({
+    status: 'error',
+    message: 'This route not yet defined'
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(501).json({
+    status: 'error',
+    message: 'This route not yet defined'
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(501).json({
+    status: 'error',
+    message: 'This route not yet defined'
+  });
+};
 
 app
   .route('/api/v1/tours')
@@ -102,6 +131,17 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/users:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 const port = 3000;
 app.listen(port, () => {
