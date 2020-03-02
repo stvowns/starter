7c4663f073dfcd6b550400f775729e3f031d0036
Oktay Z <email>
2020-03-03 00:05:55 +0300

    Chaining multiple middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+
 const router = express.Router();
 
 router.param('id', tourController.checkID);
@@ -7,7 +8,7 @@ router.param('id', tourController.checkID);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
