2765bf46d4971215af8baf22088b4c16a2199460
Oktay Z <email>
2020-03-07 18:53:46 +0300

    113 global Error Handling

--- app.js
+++ app.js
@@ -23,4 +23,21 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!!!`
+  });
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.msg
+  });
+});
+
 module.exports = app;
